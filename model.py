19cdacca997a4a0c784ac717d1c2b293aa70e48d
Swadhin Paul <41795181+swadhinbubaipaul@users.noreply.github.com>
2021-07-23 13:44:51 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,81 +1,99 @@
 import torch
 import torch.nn as nn
 
-#print(torch.cuda.get_device_name(0))
+class double_conv(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
 
-def double_conv(input_ch, output_ch):
-        conv = nn.Sequential(
-        nn.Conv2d(input_ch, output_ch, kernel_size=3),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(output_ch, output_ch, kernel_size=3),
-        nn.ReLU(inplace=True))
-        return conv
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_c)
 
-def crop_image(tensor, target_tensor):
-    target_size = target_tensor.size()[2]
-    tensor_size = tensor.size()[2]
-    delta = tensor_size - target_size
-    delta = delta // 2
-    return tensor[:, :, delta:tensor_size-delta, delta:tensor_size-delta]
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_c)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, inputs):
+        x = self.conv1(inputs)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        return x
+
+class encoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv = double_conv(in_c, out_c)
+        self.pool = nn.MaxPool2d((2, 2))
+
+    def forward(self, inputs):
+        x = self.conv(inputs)
+        p = self.pool(x)
+
+        return x, p
+
+class decoder_block(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
+        self.conv = double_conv(out_c+out_c, out_c)
+
+    def forward(self, inputs, skip):
+        x = self.up(inputs)
+        x = torch.cat([x, skip], axis=1)
+        x = self.conv(x)
+        return x
 
 class UNet(nn.Module):
     def __init__(self):
-        super(UNet, self).__init__()
-        self.max_pool_2x2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.down_conv1 = double_conv(1,64)
-        self.down_conv2 = double_conv(64,128)
-        self.down_conv3 = double_conv(128,256)
-        self.down_conv4 = double_conv(256,512)
-        self.down_conv5 = double_conv(512,1024)
-        
-        self.uptrans1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
-        self.up_conv1 = double_conv(1024, 512)
-        
-        self.uptrans2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
-        self.up_conv2 = double_conv(512, 256)
-        
-        self.uptrans3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
-        self.up_conv3 = double_conv(256, 128)
-        
-        self.uptrans4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
-        self.up_conv4 = double_conv(128, 64)
-        
-        self.out = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1)
-        
-    def forward(self, image):
-        #Encoder part
-        x1 = self.down_conv1(image)
-        x2 = self.max_pool_2x2(x1)
-        x3 = self.down_conv2(x2)
-        x4 = self.max_pool_2x2(x3)
-        x5 = self.down_conv3(x4)
-        x6 = self.max_pool_2x2(x5)
-        x7 = self.down_conv4(x6)
-        x8 = self.max_pool_2x2(x7)
-        x9 = self.down_conv5(x8)
-        
-        #Decoder part
-        x = self.uptrans1(x9)
-        y = crop_image(x7,x)
-        x = self.up_conv1(torch.cat([x, y], 1))
-        
-        x = self.uptrans2(x)
-        y = crop_image(x5,x)
-        x = self.up_conv2(torch.cat([x, y], 1))
-        
-        x = self.uptrans3(x)
-        y = crop_image(x3,x)
-        x = self.up_conv3(torch.cat([x, y], 1))
-        
-        x = self.uptrans4(x)
-        y = crop_image(x1,x)
-        x = self.up_conv4(torch.cat([x, y], 1))
-        
-        x = self.out(x)
-        #print(x.size())
-        return x
+        super().__init__()
+
+        """ Encoder """
+        self.e1 = encoder_block(3, 64)
+        self.e2 = encoder_block(64, 128)
+        self.e3 = encoder_block(128, 256)
+        self.e4 = encoder_block(256, 512)
+
+        """ Bottleneck """
+        self.b = double_conv(512, 1024)
+
+        """ Decoder """
+        self.d1 = decoder_block(1024, 512)
+        self.d2 = decoder_block(512, 256)
+        self.d3 = decoder_block(256, 128)
+        self.d4 = decoder_block(128, 64)
+
+        """ Classifier """
+        self.outputs = nn.Conv2d(64, 1, kernel_size=1, padding=0)
+
+    def forward(self, inputs):
+        """ Encoder """
+        s1, p1 = self.e1(inputs)
+        s2, p2 = self.e2(p1)
+        s3, p3 = self.e3(p2)
+        s4, p4 = self.e4(p3)
+
+        """ Bottleneck """
+        b = self.b(p4)
+
+        """ Decoder """
+        d1 = self.d1(b, s4)
+        d2 = self.d2(d1, s3)
+        d3 = self.d3(d2, s2)
+        d4 = self.d4(d3, s1)
+
+        outputs = self.outputs(d4)
+
+        return outputs
 
 if __name__ == "__main__":
-    image = torch.rand(1, 1, 572, 572)
-    model = UNet()
-    print(model(image))
+    x = torch.randn((2, 3, 512, 512))
+    f = UNet()
+    y = f(x)
+    print(y.shape)
